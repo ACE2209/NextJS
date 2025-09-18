ad4438978381c86d99f745413e5bb65da069f789
Truan dep troai <trantnce180829@fpt.edu.vn>
2025-09-18 14:02:36 +0700

    update next_shadcn

--- nextjs/app/about/page.tsx
+++
@@ -1,9 +0,0 @@
-// src/app/about/page.tsx
-export default function About() {
-  return (
-    <div>
-      <h1>Giới thiệu</h1>
-      <p>Đây là trang About, tự động nằm trong layout gốc.</p>
-    </div>
-  );
-}
